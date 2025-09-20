eadaf550375f223ff90c7f50d6731ef5fd763c94
clintonthedeveloper <ochiengclinton95@gmail.com>
2025-09-19 22:49:14 -0700

    initial commit

---
+++ employee/models.py
@@ -0,0 +1,8 @@
+# from django.db import models
+
+# # Create your models here.
+# class Employee(models.Model):
+#     employee_id = models.CharField(max_lenth=20)
+#     employee_name = models.CharField(max_lenth=100)
+#     employee_email = models.EmailField()
+#     employee_contact = models.CharField(max_lenth=15)
